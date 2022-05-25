9919ba70a5455324bf15cc774a14496794053766
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-05-25 12:20:04 -0700

    fix the logo size

--- src/icons/Whale.tsx
+++ src/icons/Whale.tsx
@@ -20,7 +20,7 @@ export default function ({ large }: { large?: boolean }) {
     <svg
       width={large ? '124' : '62'}
       height={large ? '110' : '55'}
-      viewBox={large ? '0 0 124 110' : '0 0 62 55'}
+      viewBox="0 0 62 55"
       fill="none"
       className={dropShadow('drop-shadow-primary')}
       xmlns="http://www.w3.org/2000/svg"
