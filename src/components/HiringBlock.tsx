d217af312befe9cbbf98d81998ff949f726bd747
Daniil <dpancov64@gmail.com>
2022-05-30 21:28:52 +0300

    added mailto link

--- src/components/HiringBlock.tsx
+++ src/components/HiringBlock.tsx
@@ -58,7 +58,13 @@ export default function () {
         <HeaderText small center>
           Interested in joining the team or want to reach out?
         </HeaderText>
-        <Button outlined>Message us</Button>
+        <Button
+          outlined
+          url="hi@bwl.gg"
+          onClick={() => window.open('mailto:hi@bwl.gg')}
+        >
+          Message us
+        </Button>
       </div>
     </div>
   )
