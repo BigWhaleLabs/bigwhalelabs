3aab1701a17d0d31e4005261fceb7e08a4b9c836
Daniil <dpancov64@gmail.com>
2022-05-30 19:03:49 +0300

    sealcred block height finished

--- src/components/SealCred.tsx
+++ src/components/SealCred.tsx
@@ -6,31 +6,52 @@ import {
 } from 'components/Text'
 import Button from 'components/Button'
 import SealCredLogo from 'icons/SealCredLogo'
-import classnames, { maxWidth, space } from 'classnames/tailwind'
+import classnames, {
+  display,
+  flexDirection,
+  justifyContent,
+  margin,
+  maxWidth,
+  space,
+  width,
+} from 'classnames/tailwind'
+import PseudoSuit from 'icons/PseudoSuit'
 
+const cardWrapper = classnames(
+  display('flex'),
+  flexDirection('flex-row'),
+  justifyContent('justify-between'),
+  width('w-3/4'),
+  margin('mr-72')
+)
 const card = classnames(space('space-y-2'), maxWidth('max-w-400'))
 const cardHeader = space('space-y-4')
 const cardBody = space('space-y-6')
 
 export default function () {
   return (
-    <div className={card}>
-      <div className={cardHeader}>
-        <SealCredLogo />
-        <PrimaryAccentText color="text-accent">SealCred</PrimaryAccentText>
+    <div className={cardWrapper}>
+      <div className={card}>
+        <div className={cardHeader}>
+          <SealCredLogo />
+          <PrimaryAccentText color="text-accent">SealCred</PrimaryAccentText>
+        </div>
+        <div className={cardBody}>
+          <HeaderText small>
+            Verify NFT ownership while remaining anonymous with ZK badges.
+          </HeaderText>
+          <BodyText>
+            <AccentText color="text-accent">SealCred</AccentText> allows you to
+            experience the world pseudonymously with zero knowledge proof (zk
+            badges). This means you can prove ownership of an NFT without it
+            tracing back to you.
+          </BodyText>
+          <Button outlined url="https://sealcred.xyz/">
+            Get started
+          </Button>
+        </div>
       </div>
-      <div className={cardBody}>
-        <HeaderText small>
-          Verify NFT ownership while remaining anonymous with ZK badges.
-        </HeaderText>
-        <BodyText>
-          <AccentText color="text-accent">SealCred</AccentText> allows you to
-          experience the world pseudonymously with zero knowledge proof (zk
-          badges). This means you can prove ownership of an NFT without it
-          tracing back to you.
-        </BodyText>
-        <Button outlined>Get started</Button>
-      </div>
+      <PseudoSuit />
     </div>
   )
 }
