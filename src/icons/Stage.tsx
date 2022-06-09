311a351390d41034fb3e6a3eff1b46ce8ec02ef6
Daniil <dpancov64@gmail.com>
2022-06-09 17:07:56 +0300

    remade stage with css, should be a little faster

--- src/icons/Stage.tsx
+++ src/icons/Stage.tsx
@@ -1,29 +1,34 @@
-import { scale, stroke } from 'classnames/tailwind'
+import classnames, {
+  borderColor,
+  borderStyle,
+  borderWidth,
+  height,
+  scale,
+  width,
+} from 'classnames/tailwind'
 import scrollAnimationProvider from 'helpers/scrollAnimationProvider'
 import useBreakpoints from 'hooks/useBreakpoints'
+
+const ellipseStyles = (xs: boolean) =>
+  classnames(
+    height('h-20'),
+    width('w-64'),
+    scale(xs ? 'scale-75' : 'scale-100'),
+    borderStyle('border-dashed'),
+    borderWidth('border-2'),
+    borderColor('border-primary-semi-dimmed')
+  )
 
 export default function () {
   const { xs } = useBreakpoints()
 
   return (
-    <svg
-      style={scrollAnimationProvider('walletAndStageAnimation')}
-      width="258"
-      height="77"
-      viewBox="0 0 258 77"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={scale(xs ? 'scale-75' : 'scale-100')}
-    >
-      <ellipse
-        cx="129"
-        cy="38.5"
-        rx="128"
-        ry="37.5"
-        className={stroke('stroke-primary-semi-dimmed')}
-        stroke-linecap="round"
-        stroke-dasharray="5 5"
-      />
-    </svg>
+    <div
+      className={ellipseStyles(xs)}
+      style={{
+        borderRadius: '50%',
+        ...scrollAnimationProvider('walletAndStageAnimation'),
+      }}
+    />
   )
 }
