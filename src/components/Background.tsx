1b9a3d61c25a956ecf8b25176fe45946dc7c9e56
Daniil <dpancov64@gmail.com>
2022-06-13 13:18:59 +0300

    created fadeIn-out animation basis

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,21 +1,34 @@
-import classnames, { height, position, width } from 'classnames/tailwind'
+import classnames, {
+  height,
+  opacity,
+  position,
+  transitionDuration,
+  transitionProperty,
+  width,
+} from 'classnames/tailwind'
+import useBgFade from 'hooks/useBgFade'
 import useBreakpoints from 'hooks/useBreakpoints'
 
-const bgFit = classnames(
-  position('absolute'),
-  width('w-full'),
-  height('h-full')
-)
+const bgFit = (fadeIn?: boolean) =>
+  classnames(
+    position('absolute'),
+    width('w-full'),
+    height('h-full'),
+    opacity(fadeIn ? 'opacity-100' : 'opacity-0'),
+    transitionProperty('transition-opacity'),
+    transitionDuration('duration-2000')
+  )
 
 export default function () {
   const { onlyPhones } = useBreakpoints()
+  const { fadeIn } = useBgFade()
 
   return (
     <img
       src={
         onlyPhones ? '/img/blurry-shadows-sm.svg' : '/img/blurry-shadows-lg.svg'
       }
-      className={bgFit}
+      className={bgFit(fadeIn)}
     />
   )
 }
