4cd2e3e01f5e391afdff34712dae2f75307b3512
Daniil <dpancov64@gmail.com>
2022-06-22 21:45:56 +0300

    Make first block be h-sreen and adaptive

--- src/components/StartBlock.tsx
+++ src/components/StartBlock.tsx
@@ -12,7 +12,6 @@ import classnames, {
   inset,
   justifyContent,
   margin,
-  maxWidth,
   overflow,
   position,
   scale,
@@ -23,54 +22,55 @@ import classnames, {
   wordBreak,
 } from 'classnames/tailwind'
 
+// 280-400 -- whale nose
+
 const wrapper = classnames(
   position('relative'),
   overflow('md:overflow-visible', 'overflow-hidden'),
-  width('md:w-fit', 'w-screen')
+  width('w-full'),
+  height('h-screen'),
+  display('flex'),
+  flexDirection('flex-col'),
+  justifyContent('justify-end'),
+  alignItems('items-center'),
+  margin('-mt-24')
 )
 const futureBoxWrapper = classnames(
-  position('md:absolute'),
-  inset('xl:-right-28', 'right-8', 'top-3/4')
+  position('2xl:absolute'),
+  inset('right-64', 'top-3/4')
 )
 const futureBox = classnames(
   display('flex'),
-  flexDirection('md:flex-row', 'flex-col-reverse'),
+  flexDirection('2xl:flex-row', 'flex-col-reverse'),
   alignItems('items-center'),
   justifyContent('justify-end'),
-  space('md:space-x-4'),
-  margin('mt-32', 'sm:mt-40', 'lg:mt-0')
+  space('2xl:space-x-4')
 )
 const textBlock = classnames(
   display('flex'),
   flexDirection('flex-col'),
-  alignItems('md:items-start', 'items-center'),
+  alignItems('xl:items-start', 'items-center'),
   wordBreak('break-words'),
-  textAlign('md:text-left', 'text-center')
+  textAlign('xl:text-left', 'text-center')
 )
 const bigWhaleStyles = classnames(
-  height('sm:h-auto', 'h-max'),
-  maxWidth('md:max-w-full', 'max-w-2xl'),
+  height('h-fit'),
+  width('w-fit'),
   overflow('overflow-clip'),
-  margin('md:!ml-0', 'se:ml-24', 'ml-0', 'lg:!-mt-16', 'se:mt-0', '-mt-24'),
+  margin('tablet:mr-0', '-mr-96'),
   scale('md:!scale-100', 'se:scale-125', 'scale-100')
 )
 
 const bgImage = classnames(position('absolute'), blur('blur-sm'))
 const leftGem = classnames(
   bgImage,
-  inset('md:top-1/3', 'md:-left-96'),
+  inset('md:top-1/3', 'xl:left-8', 'right-3'),
   brightness('brightness-75'),
   visibility('md:visible', 'invisible')
 )
 const rightGem = classnames(
   bgImage,
-  inset(
-    'xl:top-12',
-    'xl:!-right-1/5',
-    'md:right-2/3',
-    '-bottom-11',
-    '-right-44'
-  ),
+  inset('xl:top-12', 'xl:!right-3', 'md:right-2/3', '-bottom-11', '-right-44'),
   scale('scale-50'),
   brightness('brightness-75')
 )
@@ -89,27 +89,27 @@ export default function () {
   return (
     <div className={wrapper}>
       <ParallaxProvider>
-        <Parallax speed={25}>
-          <img src="/img/gem.webp" className={leftGem} />
-          <img src="/img/gem.webp" className={rightGem} />
-          <div className={ironSphere}>
-            <IronSphere />
+        {/* <Parallax speed={25}> */}
+        <img src="/img/gem.webp" className={leftGem} />
+        <img src="/img/gem.webp" className={rightGem} />
+        <div className={ironSphere}>
+          <IronSphere />
+        </div>
+        {/* <Parallax speed={-20}> */}
+        <img src="/img/colorful-whale.webp" className={bigWhaleStyles} />
+        {/* </Parallax> */}
+        <div className={futureBoxWrapper}>
+          {/* <Parallax speed={-12}> */}
+          <div className={futureBox}>
+            <DownArrows />
+            <div className={textBlock}>
+              <FoldText>We’re building the future</FoldText>
+              <FoldText>of pseudonymity</FoldText>
+            </div>
           </div>
-          <Parallax speed={-20}>
-            <img src="/img/colorful-whale.webp" className={bigWhaleStyles} />
-          </Parallax>
-          <div className={futureBoxWrapper}>
-            <Parallax speed={-12}>
-              <div className={futureBox}>
-                <DownArrows />
-                <div className={textBlock}>
-                  <FoldText>We’re building the future</FoldText>
-                  <FoldText>of pseudonymity</FoldText>
-                </div>
-              </div>
-            </Parallax>
-          </div>
-        </Parallax>
+          {/* </Parallax> */}
+        </div>
+        {/* </Parallax> */}
       </ParallaxProvider>
     </div>
   )
