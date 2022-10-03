be4efdc1838c716ce2d69f1e39285406c2f1d303
Daniil <dpancov64@gmail.com>
2022-10-03 20:06:39 +0300

    Begun optimizing backgrounds

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
         <HiringBlock />
         <Octocorp />
         <LogoFooter />
-        <Background />
+        {/* <Background /> */}
         <Footer />
       </Root>
     </>
--- src/components/PrivacyWorld.tsx
+++ src/components/PrivacyWorld.tsx
@@ -1,12 +1,15 @@
 import { LargeStrokeText } from 'components/Text'
+import SectionBackground from 'components/SectionBackground'
 import classnames, {
   margin,
   maxWidth,
+  position,
   wordBreak,
   zIndex,
 } from 'classnames/tailwind'
 
 const privacyWorld = classnames(
+  position('relative'),
   margin('md:my-60', 'my-32'),
   wordBreak('break-words'),
   maxWidth('max-w-full'),
@@ -20,6 +23,14 @@ export default function () {
         We’re dedicated to building a pseudonymous WORLD WHERE privacy and
         identity are owned by the human, not the corporation.
       </LargeStrokeText>
+      <SectionBackground
+        width={2759}
+        height={744}
+        background="#00F3F9"
+        bottom={0}
+        left={-50}
+        rotate={23}
+      />
     </div>
   )
 }
