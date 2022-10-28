9287c5c334fb2501a88f04747c9583e927b5beb0
Daniil <dpancov64@gmail.com>
2022-10-28 19:25:07 +0300

    Desktop bg for dosu

--- src/components/Projects/Dosu.tsx
+++ src/components/Projects/Dosu.tsx
@@ -1,4 +1,5 @@
 import { AccentText } from 'components/Text'
+import Background from 'components/Background'
 import DosuGraphicsBlock from 'components/DosuGraphicsBlock'
 import ProjectBlock from 'components/Projects/ProjectBlock'
 
@@ -6,6 +7,49 @@ const DosuDescription = () => (
   <>
     <AccentText color="text-primary">Dosu</AccentText> will be a pseudonymous
     social network where you can share your thoughts with the world.
+  </>
+)
+
+const Backgrounds = () => (
+  <>
+    <Background
+      width={400}
+      height={560}
+      background="#FF0095"
+      bottom={0}
+      left={-15}
+      blur={120}
+      inView={true}
+    />
+    <Background
+      width={500}
+      height={300}
+      background="#FFCDFF"
+      bottom={0}
+      left={-10}
+      blur={100}
+      inView={true}
+    />
+    <Background
+      width={900}
+      height={500}
+      background="#8349FF"
+      bottom={10}
+      left={-5}
+      rotate={-30}
+      blur={100}
+      inView={true}
+    />
+    <Background
+      width={700}
+      height={400}
+      background="#0013FF"
+      bottom={20}
+      left={20}
+      rotate={-10}
+      blur={100}
+      inView={true}
+    />
   </>
 )
 
@@ -19,6 +63,7 @@ export default function () {
       buttonTitle="Join Discord for updates"
       buttonUrl="https://discord.gg/NHk96pPZUV"
       imageElement={<DosuGraphicsBlock />}
+      additionalBackground={<Backgrounds />}
       centered
       wrapReverse
     />
