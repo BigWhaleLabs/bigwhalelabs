2ddcd0d32514bbf1298bb9646101e15c9f95fcab
T_DaMER <dpancov64@gmail.com>
2023-11-14 21:08:36 +0300

    Add whitepaper link

--- src/components/BwlLinks.tsx
+++ src/components/BwlLinks.tsx
@@ -1,9 +1,15 @@
 import { LinkText } from 'components/Text'
-import { bwlBlog, bwlGitHub, contactUs } from 'helpers/constants'
+import {
+  bwlBlog,
+  bwlGitHub,
+  contactUs,
+  whitepaperLink,
+} from 'helpers/constants'
 
 export default function () {
   return (
     <>
+      <LinkText url={whitepaperLink}>Whitepaper</LinkText>
       <LinkText url={bwlBlog}>Blog</LinkText>
       <LinkText url={bwlGitHub}>GitHub</LinkText>
       <LinkText url={contactUs}>Contact</LinkText>
